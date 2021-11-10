fb84d679c040c55ff4dd0a560c0f7320e2ec7006
lioujg <lioujg870117@gmail.com>
2021-11-10 18:31:00 +0800

    first commit

---
+++ offb/src/force_control_client.cpp
@@ -0,0 +1,57 @@
+﻿#include "ros/ros.h"
+// #include "forces_torques/"
+#include <gazebo_msgs/ApplyBodyWrench.h>
+#include <cstdlib>
+#include <geometry_msgs/Wrench.h>
+#include <string>
+#include "std_msgs/String.h"
+
+geometry_msgs::Wrench robot_cmd;
+
+void robot_wrench_cb(const geometry_msgs::Wrench::ConstPtr &msg){
+  robot_cmd = *msg;
+}
+
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "force_control_client");
+  ros::NodeHandle n;
+  std::string robot_number;
+  n.getParam("robot_number", robot_number);
+
+  ros::Subscriber robot_cmd_sub = n.subscribe<geometry_msgs::Wrench>("robot_wrench",4,robot_wrench_cb);
+
+  ros::ServiceClient wrenchClient = n.serviceClient<gazebo_msgs::ApplyBodyWrench>("/gazebo/apply_body_wrench");
+  gazebo_msgs::ApplyBodyWrench::Request apply_wrench_req;
+  gazebo_msgs::ApplyBodyWrench::Response apply_wrench_resp;
+
+  // ros::Time time_temp(0, 0);
+  // ros::Duration duration_temp(0, 1000000);
+  // apply_wrench_req.start_time = time_temp;
+  // apply_wrench_req.duration = duration_temp;
+
+  apply_wrench_req.start_time = ros::Time(0);
+  apply_wrench_req.duration = ros::Duration(-1);
+  apply_wrench_req.body_name = "payload::payload_link_" + robot_number;
+
+  bool call_service;
+  ros::Rate loop_rate(20);
+
+  while(ros::ok()){
+    apply_wrench_req.wrench.force.x = robot_cmd.force.x;
+    apply_wrench_req.wrench.force.y = robot_cmd.force.y;
+    apply_wrench_req.wrench.force.z = robot_cmd.force.z;
+
+    apply_wrench_req.wrench.torque.z = robot_cmd.torque.z;
+    apply_wrench_req.wrench.torque.z = robot_cmd.torque.z;
+    apply_wrench_req.wrench.torque.z = robot_cmd.torque.z;
+
+  	call_service = wrenchClient.call(apply_wrench_req, apply_wrench_resp);
+    loop_rate.sleep();
+    ros::spinOnce();
+  }
+
+  return 0;
+}
---
+++ offb/src/force_controller.cpp
@@ -0,0 +1,33 @@
+﻿#include "ros/ros.h"
+#include <cstdlib>
+#include <geometry_msgs/Wrench.h>
+#include <sensor_msgs/Imu.h>
+
+geometry_msgs::Wrench robot_cmd;
+sensor_msgs::Imu payload_imu;
+
+void payload_orientation_cb(const sensor_msgs::Imu::ConstPtr &msg){
+  payload_imu = *msg;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "force_controller");
+  ros::NodeHandle nh;
+
+  ros::Subscriber payload_imu_sub = nh.subscribe<sensor_msgs::Imu>("/payload/IMU",4,payload_orientation_cb);
+  ros::Publisher robot_controller_pub = nh.advertise<geometry_msgs::Wrench>("robot_wrench",4);
+  ros::Rate loop_rate(20);
+
+  while(ros::ok()){
+
+    // robot_cmd.force.z = 0.5 / 2 * 9.81;
+    robot_cmd.torque.z = 3;
+
+  	robot_controller_pub.publish(robot_cmd);
+    loop_rate.sleep();
+    ros::spinOnce();
+  }
+
+  return 0;
+}
